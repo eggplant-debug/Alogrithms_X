dea81b368d87d515eee5a7a1fe6230a4fc9aafeb
eggplant-debug <18391458847@163.com>
2021-03-28 23:10:08 +0800

    #

--- Permutation.java
+++ Permutation.java
@@ -1,5 +1,17 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdRandom;
+
 public class Permutation {
     public static void main(String[] args) {
+        int num=StdIn.readInt();
+        RandomizedQueue<String> a = new RandomizedQueue<String>();
 
-    }
+        while(!StdIn.isEmpty()){
+            a.enqueue(StdIn.readString());
+        }
+        for(int i=0;i<num;i++){
+            System.out.println(a.dequeue());
+        }
+
+}
 }
